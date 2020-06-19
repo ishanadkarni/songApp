6fc9d450cce587bbd258a6d63216b37be3eea979
Isha Nadkarni <ishvaria@gmail.com>
2020-06-19 09:40:50 -0700

    started SongApp

--- SongApp/ContentView.swift
+++ SongApp/ContentView.swift
@@ -11,10 +11,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-        Text("Hello, World!")
-        Text("Welcome to the Sound App")
-    }
-}
+        Spacer()
+            Text("Welcome to the Sound App!").font(Font.custom("AvenirNextCondensed-Bold", size: 25))
+            Text("Click one of the Vinyl records to play a song.").font(Font.custom("AvenirNextCondensed-Bold", size: 18))
+        Spacer()
+            Image("djImage").resizable().aspectRatio(contentMode: .fit)
+        Spacer()
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
+            }
+            }//Hstack end
+        Spacer()
+    }//vstack end
+} //body end
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
