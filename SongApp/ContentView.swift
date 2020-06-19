54a4368b6582154653c282360805ca625ca843d3
Isha Nadkarni <ishvaria@gmail.com>
2020-06-19 09:09:51 -0700

    Initial Commit

---
+++ SongApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SongApp
+//
+//  Created by Isha Nadkarni on 6/19/20.
+//  Copyright © 2020 Isha Nadkarni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
