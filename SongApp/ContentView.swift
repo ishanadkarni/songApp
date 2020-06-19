60ece058eade973cfc67605c7200246cdf3eacd9
Isha Nadkarni <ishvaria@gmail.com>
2020-06-19 10:22:09 -0700

    started SongApp

--- SongApp/ContentView.swift
+++ SongApp/ContentView.swift
@@ -7,24 +7,36 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    
+    @State var song: AVAudioPlayer?
+    
+    @State var songs = ["Still Corners - Fireflies.mp3", "Alkaline Trio - Clavicle.mp3", "Blackbird Blackbird - Pure.mp3"]
+    
     var body: some View {
         VStack {
         Spacer()
             Text("Welcome to the Sound App!").font(Font.custom("AvenirNextCondensed-Bold", size: 25))
-            Text("Click one of the Vinyl records to play a song.").font(Font.custom("AvenirNextCondensed-Bold", size: 18))
+            Text("Click one of the Vinyl records to play a song.").font(Font.custom("AvenirNextCondensed-Bold", size: 18)).padding(.top)
         Spacer()
             Image("djImage").resizable().aspectRatio(contentMode: .fit)
         Spacer()
         HStack {
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: {
+                self.playSound(soundNumber : 1)
+            }) {
                 Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
             }
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: {
+                self.playSound(soundNumber : 2)
+            }) {
                 Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
             }
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Button(action: {
+                self.playSound(soundNumber : 3)
+            }) {
                 Image("vinylRecord").resizable().aspectRatio(contentMode: .fit)
             }
             }//Hstack end
@@ -32,9 +44,21 @@ struct ContentView: View {
     }//vstack end
 } //body end
 
+    func playSound(soundNumber : Int) {
+        let path = Bundle.main.path(forResource: songs[soundNumber - 1], ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            song = try AVAudioPlayer(contentsOf: url)
+            song?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }//end func
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-}
+
